227feab7d85cb2b1d51a9e390512e7737e0f0cf2
Tretyak Igor <TretyakIgor2603@gmail.com>
2018-07-25 14:47:14 +0300

    added section how we are working

--- source/components/_pages/EntrepreneurSeekingFunding/EntrepreneurSeekingFunding.index.js
+++ source/components/_pages/EntrepreneurSeekingFunding/EntrepreneurSeekingFunding.index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import HowWorking from '../../entrepreneurSeekingFunding/HowWorking/HowWorking.index'
 
 function EntrepreneurSeekingFunding() {
 
   return (
     <div>
-      EntrepreneurSeekingFunding page
+      <HowWorking />
     </div>
   )
 
