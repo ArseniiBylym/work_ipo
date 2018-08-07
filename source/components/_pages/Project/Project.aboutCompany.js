2da985c6069f17be60a35f6f9e41e1f35aa700a4
Tretyak Igor <TretyakIgor2603@gmail.com>
2018-08-07 16:34:16 +0300

    added project-page about company section

--- source/components/_pages/Project/Project.aboutCompany.js
+++ source/components/_pages/Project/Project.aboutCompany.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import ReactPlayer from 'react-player'
 
 ProjectAboutCompany.propTypes = {}
 
 function ProjectAboutCompany(props) {
 
   return (
-    <div>
-      ProjectAboutCompany
-    </div>
+    <section className="project-page__section">
+      <h1 className="project-page__subtitle">
+        About our company
+      </h1>
+      <div className="project-page__video-about-company">
+        <ReactPlayer url={`https://www.youtube.com/watch?v=aa5BUDKgzRQ`}
+          width={400}
+          height={250}
+        />
+      </div>
+    </section>
   )
 
 }
