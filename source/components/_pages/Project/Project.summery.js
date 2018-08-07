9d96fad56c24169959b2dfd9201e54c05fa0b256
Tretyak Igor <TretyakIgor2603@gmail.com>
2018-08-07 16:30:32 +0300

    added project-page summery section

--- source/components/_pages/Project/Project.summery.js
+++ source/components/_pages/Project/Project.summery.js
@@ -6,9 +6,14 @@ ProjectSummery.propTypes = {}
 function ProjectSummery(props) {
 
   return (
-    <div>
-      ProjectSummery
-    </div>
+    <section className="project-page__section">
+      <h1 className="project-page__subtitle">
+        Project summery
+      </h1>
+      <div className="project-page__text">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </div>
+    </section>
   )
 
 }
