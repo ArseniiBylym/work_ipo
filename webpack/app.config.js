929d9dad7b6170cd7bd4db64277c7cfb79a1583d
Igor Tretyak <TretyakIgor2603@gmail.com>
2018-09-05 14:30:45 +0300

    rename build

--- webpack/app.config.js
+++ webpack/app.config.js
@@ -3,5 +3,5 @@ const path = require(`path`)
 module.exports = Object.freeze({
   APP_TITLE: `IPO`, // title in index.html (html-webpack-plugin)
   SRC_DIR: path.resolve(__dirname, `../source`), // directory name for development
-  PROD_DIR: path.resolve(__dirname, `../finished-product`) // directory name for production
+  PROD_DIR: path.resolve(__dirname, `../build`) // directory name for production
 })
