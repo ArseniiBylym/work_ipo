199462fb214b4d8f970b2c7d3f030fbcd4594620
Igor Tretyak <tretyakigor2603@gmail.com>
2018-09-10 14:47:30 +0300

    project grid dividing

--- source/components/_pages/Dashboard/Header/index.js
+++ source/components/_pages/Dashboard/Header/index.js
@@ -1,71 +1,36 @@
-import React, { Component, Fragment } from 'react';
-import Logo from '../../../PageHeader/PageLogo/PageLogo.index';
-import Search from './Search';
-import ProfileMenu from './ProfileMenu';
-import Langs from './LangSwitch';
-import { getAllProjects } from '../../../../redux/reducers/getProjects.reducer';
-import { resetPageContent } from '../../../../redux/reducers/pageContent.reducer';
-import './header.styl';
-import { connect } from 'react-redux';
-import multiLang from '../../../_HOC/lang.hoc'
-import { projects } from '../../../../utils/routesBack';
-import MainNav from '../../../PageHeader/MainNav/MainNav.index'
+import React, { Component } from 'react';
+import InvetorHeader from './InvestorHeader';
+import EnterpreneurHeader from './HeaderEnterpreneur';
+import Loader from '../partials/Loader';
 
 class Header extends Component {
 
-  getData = () => {
-    const {lang, dir, getAllProjects} = this.props
-    const projects = `enterpreneur/${window.localStorage.getItem('user-id')}/myprojects`
-    getAllProjects(lang, projects)
-  }
+  render() {
+    const userType = window.localStorage.getItem('user-type');
 
-  renderPage() {
-    const {content, lang} = this.props
-    
-    if(!content.pageContent) return null
-    console.log(content.pageContent[0].media.logo)
-    const pageHeaderText = content.pageContent[2][lang]
-  
+    let content;
+
+    switch (userType) {
+      case 'investor': {
+        content = <InvetorHeader />;
+        break;
+      }
+
+      case 'enterpreneur': {
+        content = <EnterpreneurHeader />
+        break;
+      }
+
+      default: {
+        content = <Loader />
+      }
+    }
+
     return (
-      <header className="page-header dash-header">
-        <Logo
-          path="dash"
-          logo={content.pageContent[0].media.logo}
-        />
-        <div className='main_nav-wrapper'>
-          <MainNav contentText = {pageHeaderText}/>
-        </div>
-        <div className="dash-header__menu">
-          <Search placeholder={content.pageContent[0][lang].search}/>
-          <div className="dash-header__settings">
-            <div className="dash-header__lang">
-              <Langs
-                langs={content.pageContent[0]}
-                getData={this.getData}
-              />
-            </div>
-            <ProfileMenu />
-          </div>
-        </div>
-      </header>
+      <div>{content}</div>
     );
   }
-  render () {
-    return(
-      <Fragment>
-        {this.renderPage()}
-      </Fragment>
-      )
-  }
 
 }
 
-const mapStateToProps = state => {
-  return {
-    content: state.allProjects
-  }
-}
-
-export default connect(mapStateToProps, { getAllProjects })(
-  multiLang(Header)
-  );
+export default Header;
