77bb29a3c9a138feb92f76f3adfca2cb395674d9
CubeX <agent150891@gmail.com>
2018-09-10 10:22:14 +0300

    added subscribe button

--- source/utils/routesBack.js
+++ source/utils/routesBack.js
@@ -1,4 +1,8 @@
+import axios from "axios"
+
 export const BASE_URL = !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? 'http://192.168.88.170:3000' : 'http://34.199.42.221:3000'
+window.localStorage.getItem(`user-token`)? axios.defaults.headers.common[`token`] = window.localStorage.getItem(`user-token`) : null
+
 
 export const contacts = `contactus`
 export const about = `aboutus`
